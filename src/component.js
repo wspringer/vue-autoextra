9a1cd6279ae046c28d5dc86e78db2000b63fee2d
Wilfred Springer <wilfred@eastpole.nl>
2019-12-07 17:15:58 +0100

    Fixing linting issues

--- src/component.js
+++ src/component.js
@@ -1,40 +1,42 @@
 function isEmpty(obj) {
-  return Object.keys(obj).length === 0
+  return Object.keys(obj).length === 0;
 }
 
 export default {
   props: ['collection'],
-  data () {
+  data() {
     return {
       shadow: [],
-      placeholder: {}
-    }
+      placeholder: {},
+    };
   },
-  mounted () {    
-    this.$watch('collection', (next) => {
-      const unregister = this.$watch(
-        'placeholder',
-        (updated) => {
-          console.info('Updating')
-          if (!isEmpty(updated)) {
-            unregister()
-            if (next) {
-              next.push(this.placeholder)
-              this.placeholder = {}
+  mounted() {
+    this.$watch(
+      'collection',
+      next => {
+        const unregister = this.$watch(
+          'placeholder',
+          updated => {
+            if (!isEmpty(updated)) {
+              unregister();
+              if (next) {
+                next.push(this.placeholder);
+                this.placeholder = {};
+              }
             }
-          }
-        }, 
-        { deep: true }
-      )
-      this.shadow = next ? next.concat(this.placeholder) : [ this.placeholder ]
-    }, { immediate: true })
-  },  
+          },
+          { deep: true }
+        );
+        this.shadow = next ? next.concat(this.placeholder) : [this.placeholder];
+      },
+      { immediate: true }
+    );
+  },
   template: `
 <div>
   <div v-for="(item, index) in shadow">
     <slot :item="item" :last="index === shadow.length - 1" :index="index"/>
   </div>
 </div>
-`
-}
-
+`,
+};
--- src/component.test.js
+++ src/component.test.js
@@ -1,41 +1,44 @@
-import Autoextra from './component'
-import { mount, shallowMount } from '@vue/test-utils'
-
+import { mount, shallowMount } from '@vue/test-utils';
+import Autoextra from './component';
 
 describe('Autoextra', () => {
+  it('should include a slot for ever item in the collection, plus one extra', () => {
+    return new Promise(done => {
+      const wrapper = mount(Autoextra, {
+        propsData: {
+          collection: [
+            {
+              name: 'joe',
+            },
+          ],
+        },
+        scopedSlots: {
+          default: '<div><input type="text" v-model="props.item.name"/></div>',
+        },
+      });
+      wrapper.vm.$nextTick(() => {
+        expect(wrapper.findAll('input').length).toEqual(2);
+        expect(wrapper.findAll('input').at(0).element.value).toEqual('joe');
+        done();
+      });
+    });
+  });
 
-  it('should include a slot for ever item in the collection, plus one extra', done => {
-    const wrapper = mount(Autoextra, {
-      propsData: {
-        collection: [{
-          name: 'joe'
-        }]
-      },
-      scopedSlots: {
-        default: '<div><input type="text" v-model="props.item.name"/></div>'
-      }
-    })
-    wrapper.vm.$nextTick(() => {
-      expect(wrapper.findAll('input').length).toEqual(2)
-      expect(wrapper.findAll('input').at(0).element.value).toEqual('joe')
-      done()
-    })
-  })
-
-  it('should include yet another entry when editing the extra entry', done => {
-    const wrapper = mount(Autoextra, {
-      propsData: {
-        collection: []
-      },
-      scopedSlots: {
-        default: '<div><pre>{{props}}</pre><input type="text" v-model="props.item.name"/></div>'
-      }
-    })
-    wrapper.vm.$nextTick(() => {
-      const extraInput = wrapper.findAll('input').at(0)
-      extraInput.setValue('bar')
-      done()
-    })
-  })
-
-})
+  it('should include yet another entry when editing the extra entry', () => {
+    return new Promise(done => {
+      const wrapper = mount(Autoextra, {
+        propsData: {
+          collection: [],
+        },
+        scopedSlots: {
+          default: '<div><pre>{{props}}</pre><input type="text" v-model="props.item.name"/></div>',
+        },
+      });
+      wrapper.vm.$nextTick(() => {
+        const extraInput = wrapper.findAll('input').at(0);
+        extraInput.setValue('bar');
+        done();
+      });
+    });
+  });
+});
